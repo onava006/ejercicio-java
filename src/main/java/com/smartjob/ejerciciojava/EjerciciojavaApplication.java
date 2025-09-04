96cc29e5e7dab23e0247260c13c599ffcca9cd1c
Oscar Navarrete <oscar.nvergara@gmail.com>
2025-09-03 21:58:46 -0400

    initial commit

---
+++ src/main/java/com/smartjob/ejerciciojava/EjerciciojavaApplication.java
@@ -0,0 +1,13 @@
+package com.smartjob.ejerciciojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartjob/ejerciciojava/EjerciciojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartjob.ejerciciojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
